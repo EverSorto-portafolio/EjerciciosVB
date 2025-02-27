60bf4f84b95a66b25b03d5ea0940551bc131a953
EverSorto <everalfredo.sorto@gmail.com>
2025-02-26 18:57:36 -0600

    Mi primer commit  guargo funciones en VB de  sobre el uso del for

---
+++ ejercicioSemana6/BuclesEjercicios.vb
@@ -0,0 +1,32 @@
+﻿Public Class BuclesEjercicios
+
+    Function pasoApaso(number As Integer)
+        Dim i As Integer
+
+        For i = 0 To number
+            Console.WriteLine(i)
+        Next
+    End Function
+
+    Function forEachejemplo(num1 As Integer,
+                            num2 As Integer,
+                            num3 As Integer)
+
+        Dim miArray(2) As Integer
+        miArray(0) = num1
+        miArray(1) = num2
+        miArray(2) = num3
+
+        For Each temp In miArray
+            Dim v As Integer = temp
+            Dim valor1 = Integer.Parse(Console.ReadLine())
+            Console.WriteLine(temp & " + " & valor1)
+            Console.WriteLine(v + valor1)
+
+
+        Next
+
+
+    End Function
+
+End Class
---
+++ ejercicioSemana6/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim bucle As BuclesEjercicios = New BuclesEjercicios()
+
+        bucle.pasoApaso(5)
+        bucle.forEachejemplo(25, 10, 2)
+    End Sub
+End Module
